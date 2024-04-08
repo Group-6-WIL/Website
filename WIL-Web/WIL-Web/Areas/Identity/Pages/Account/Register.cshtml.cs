ad78618a873da0e387701d7bef8ee06d15ca75bd
Ryan-Berriman <ST10044691@vcconnect.edu.za>
2024-04-08 16:30:49 +0200

    created the register page of the website

--- WIL-Web/WIL-Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WIL-Web/WIL-Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,20 @@ namespace WIL_Web.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Surname")]
+            public string Surname { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
