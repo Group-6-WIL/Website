46f3aad1ea0c8b6c4bb0265143ed0412ddef4835
ST10044691 <104129987+Ryan-Berriman@users.noreply.github.com>
2024-07-04 16:25:15 +0200

    Delete WIL-Web directory

--- WIL-Web/WIL-Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WIL_Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
