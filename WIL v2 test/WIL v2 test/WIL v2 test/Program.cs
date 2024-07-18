6c1b0204cf0d2f3cfa3a873ad026d4dab50ec0b8
Ryan-Berriman <ST10044691@vcconnect.edu.za>
2024-07-18 09:20:59 +0200

    adjustments, edited locations page

--- WIL v2 test/WIL v2 test/WIL v2 test/Program.cs
+++ WIL v2 test/WIL v2 test/WIL v2 test/Program.cs
@@ -45,6 +45,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+
 app.MapRazorPages();
 
 // Create Admin role and assign the default user to it
